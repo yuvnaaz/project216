a13a21f6845422695fc159e379f53d7b9856213b
Apoorv Goyal <apoorvelous@gmail.com>
2021-10-14 17:57:56 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+io.on("connection", (socket) => {
+    socket.on("message", (message) => {
+        io.emit("createMessage", message);
+    });
+});
+
+server.listen(3030);
