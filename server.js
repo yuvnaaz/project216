43c8e389a6328e968edda2473e3596bd3214ca8d
yuvnaaz <69728010+yuvnaaz@users.noreply.github.com>
2023-07-28 18:24:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const io = require("socket.io")(server); 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
